0fac6b012853aff05a294d93168e81bca40ce2b1
Siraddeen <85670787+Siraddeen@users.noreply.github.com>
2024-03-06 08:18:39 +0530

    Create a-Interface.ts

---
+++ 9-2/a-Interface.ts
@@ -0,0 +1,28 @@
+// Interfaces
+
+/*
+      when you need to give type to certain things like object first create skeleton of it &
+      give types to it    then use it  as  Interface
+      now give this interface as type of object 
+*/
+
+interface User {
+  fname: string;
+  lname: string;
+  age: number;
+}
+
+function pep(user: User) {
+  if (user.age > 18) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let u = pep({
+  fname: "raj",
+  lname: "dins",
+  age: 20,
+});
+console.log(u);
