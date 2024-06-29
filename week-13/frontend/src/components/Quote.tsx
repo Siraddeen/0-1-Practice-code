0016ebcb361135c2f11383a863c74eb6d537bcc0
Siraddeen <sircoil17@gmail.com>
2024-06-29 19:32:30 +0530

    1 blog project

---
+++ week-13/frontend/src/components/Quote.tsx
@@ -0,0 +1,31 @@
+export const Quote = () => {
+  return (
+    <div className=" bg-slate-200 h-screen flex justify-center flex-col">
+      <div className=" flex justify-center">
+        <div className=" max-w-lg">
+          <div className=" text-3xl font-bold">
+            A small project for keeping your notes/blogs together in a place
+          </div>
+          <div className=" max-w-md text-xl font-semibold text-left mt-4">
+            Siraddeen
+          </div>
+          <div className=" max-w-md text-sm font-light text-slate-400">
+            in learning phase
+          </div>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <p className="italic">
+            Kindly wait for a moment to get signUp, cause backend has to wakeup
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ week-13/frontend/src/config.ts
@@ -0,0 +1,3 @@
+// export const BACKEND_URL = process.env.REACT_APP_Database_Url;
+
+export const BACKEND_URL = "https://backend.siraddeen15.workers.dev";
